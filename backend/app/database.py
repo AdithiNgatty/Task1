e16b751ef5c98adac72af0286da8accc0af85da8
AdithiNgatty <adithigatty@gmail.com>
2025-10-14 12:40:49 +0530

    dockerfile add

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,12 @@
-import motor.motor_asyncio
+import os
+from pymongo import MongoClient
 
-MONGO_URL = "mongodb://localhost:27017"
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
-db = client.user_auth_db 
+MONGO_USER = os.getenv("MONGO_INITDB_ROOT_USERNAME", "root")
+MONGO_PASS = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "example")
+MONGO_DB = os.getenv("MONGO_INITDB_DATABASE", "user_auth_db")
+MONGO_HOST = os.getenv("MONGO_HOST", "mongodb")  # Use service name from docker-compose
+
+MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:27017/{MONGO_DB}?authSource=admin"
+
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
